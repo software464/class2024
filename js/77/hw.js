26bad68248346af0473113ac52d9014c03aee33e
software464 <a2ztrading453@gmail.com>
2024-09-24 19:56:12 -0400

    added hw 77

---
+++ js/77/hw.js
@@ -0,0 +1,37 @@
+(async function () {
+    "use strict";
+    const videoHolders = document.querySelectorAll(".videoHOlder");
+    console.log(videoHolders);
+    const videoElement = document.querySelector('.videoPlayer');
+    ///fetching item
+    const response = await fetch('videos.json');
+    const videoList = await response.json();
+    ///setting img to each element
+
+    for (let index = 0; index < videoHolders.length; index++) {
+
+        videoHolders[index].children.item(0).src = `${videoList[index].img||"media/video.png"}` ;
+
+        videoHolders[index].children.item(1).innerText = `${videoList[index].title||"N/A"} `;
+
+        videoHolders[index].children.item(0).addEventListener('click', e => {
+            playVideo(`${videoList[index].src}`);
+
+        });
+
+
+
+
+    }
+    /// video function
+    function playVideo(source) {
+        console.log("playing video",source);
+        videoElement.src = source;
+        videoElement.scrollIntoView(false);
+        videoElement.play();
+    }
+
+
+
+
+}());
